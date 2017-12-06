1a693b2b63906d042e090b6587f011242ae4b04a
Greencame <julien.mustiere@ntymail.com>
2017-12-06 17:52:06 +0100

    GL-2

--- controllers/courselist-controller.js
+++ controllers/courselist-controller.js
@@ -31,4 +31,40 @@ router.post('/', (req, res, next) => {
   })
 })
 
+router.delete('/', (req, res, next) => {
+
+  if (!req.body.id && !req.body.name) {
+    return next(new BadRequestError('VALIDATION', 'Missing id or name'))
+  }
+  //ID
+  if(req.body.id){
+    const id = req.body.id;
+    const result = find(courseListCollection, {id})
+    
+    if (!result) {
+      return next(new BadRequestError('VALIDATION', 'ID should be valid'))
+    }
+
+    courseListCollection.splice(result.id - 1, 1)
+      
+  }
+  //name 
+  else {
+    const name = req.body.name;
+    const result = find(courseListCollection, {name})
+
+    if (!result) {
+      return next(new BadRequestError('VALIDATION', 'Name should be valid'))
+    }
+
+    courseListCollection.splice(result.id - 1, 1)
+  }
+
+  res.json({
+    data: {
+      result : "ok"
+    }
+  })  
+})
+
 module.exports = router
--- test/integration/courselist-controller.spec.js
+++ test/integration/courselist-controller.spec.js
@@ -8,6 +8,7 @@ const { find } = require('lodash')
 
 const db = require('../../data/db')
 const app = require('../../app')
+const url = '/course-lists'
 
 const courseListFixture = require('../fixtures/courseList')
 
@@ -17,7 +18,7 @@ describe('CourselistController', () => {
 
   describe('When I create a courseList (POST /course-lists)', () => {
     it('should reject with a 400 when no name is given', () => {
-      return request(app).post('/course-lists').then((res) => {
+      return request(app).post(url).then((res) => {
         res.status.should.equal(400)
         res.body.should.eql({
           error: {
@@ -30,7 +31,7 @@ describe('CourselistController', () => {
 
     it('should reject when name is not unique', () => {
       return request(app)
-        .post('/course-lists')
+        .post(url)
         .send({ name: 'Toto' })
         .then((res) => {
           res.status.should.equal(400)
@@ -47,7 +48,7 @@ describe('CourselistController', () => {
       const mockName = 'My New List'
 
       return request(app)
-        .post('/course-lists')
+        .post(url)
         .send({ name: mockName })
         .then((res) => {
           res.status.should.equal(200)
@@ -60,6 +61,82 @@ describe('CourselistController', () => {
             id: res.body.data.id,
             name: res.body.data.name
           })
+        })
+    })
+  })
+
+  describe('When I delete a courseList (DELETE /course-lists)', () => {
+    it('should reject with a 400 when no ID or name is given', () => {
+      return request(app).delete(url).then((res) => {
+        res.status.should.equal(400)
+        res.body.should.eql({
+          error: {
+            code: 'VALIDATION',
+            message: 'Missing id or name'
+          }
+        })
+      })
+    })
+
+    it('should reject when id does not exist', () => {
+      return request(app)
+        .delete(url)
+        .send({ id: 50 })
+        .then((res) => {
+          res.status.should.equal(400)
+          res.body.should.eql({
+            error: {
+              code: 'VALIDATION',
+              message: 'ID should be valid'
+            }
+          })
+      })
+    })
+
+    it('should reject when name does not exist', () => {
+      return request(app)
+        .delete(url)
+        .send({ name: 'Titi' })
+        .then((res) => {
+          res.status.should.equal(400)
+          res.body.should.eql({
+            error: {
+              code: 'VALIDATION',
+              message: 'Name should be valid'
+            }
+          })
+      })
+    })
+
+    it('should succesfuly delete a courseList with a id', () => {
+      const courseID = 1;
+
+      return request(app)
+        .delete(url)
+        .send({ id: courseID })
+        .then((res) => {
+          res.status.should.equal(200)
+          expect(res.body.data).to.be.an('object')
+          res.body.data.result.should.equal("ok")          
+
+          const result = find(db.courseList, { id: courseID } )
+          expect(result).to.be.an('undefined')
+        })
+    })
+
+    it('should succesfuly delete a courseList with a name', () => {
+      const courseName = "Toto";
+
+      return request(app)
+        .delete(url)
+        .send({ name: courseName })
+        .then((res) => {
+          res.status.should.equal(200)
+          expect(res.body.data).to.be.an('object')
+          res.body.data.result.should.equal("ok")
+
+          const result = find(db.courseList, { name: courseName } )
+          expect(result).to.be.an('undefined')
         })
     })
   })
